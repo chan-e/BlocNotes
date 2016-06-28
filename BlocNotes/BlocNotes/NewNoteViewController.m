d4e39693acf1e2f9d8ebfe723d2178d93e9a15d5
Eddy Chan <echan162@gmail.com>
2016-06-28 12:16:28 -0700

    Set up the notes table view for adding notes

--- BlocNotes/BlocNotes/NewNoteViewController.m
+++ BlocNotes/BlocNotes/NewNoteViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "NewNoteViewController.h"
+#import "CoreDataStack.h"
+#import "Note.h"
 
-@interface NewNoteViewController ()
+@interface NewNoteViewController () <UITextViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UILabel *placeholderTextLabel;
+@property (weak, nonatomic) IBOutlet UITextView *bodyTextView;
 
 @end
 
@@ -20,6 +25,15 @@
 }
 
 - (IBAction)donePressed:(id)sender {
+    CoreDataStack *sharedCoreDataStack = [CoreDataStack sharedStack];
+    
+    Note *note = [NSEntityDescription insertNewObjectForEntityForName:@"Note"
+                                               inManagedObjectContext:sharedCoreDataStack.managedObjectContext];
+    note.date = [NSDate date];
+    note.body = self.bodyTextView.text;
+    
+    [sharedCoreDataStack saveContext];
+    
     [self.presentingViewController dismissViewControllerAnimated:YES
                                                       completion:nil];
 }
@@ -27,11 +41,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.bodyTextView.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UITextViewDelegate
+
+- (void)textViewDidBeginEditing:(UITextView *)textView {
+    self.placeholderTextLabel.hidden = YES;
 }
 
 /*
